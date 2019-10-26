04122a11c3a0c5913c609d7f1353bcf391ba64e9
Syukur yakub <56242226+Syukuryakub@users.noreply.github.com>
2019-10-26 08:34:10 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,11 @@
+#ini variabel & nilai
+
+namalengkap = "Syukur Yakub"
+namapanggilan = "Syukur"
+npm = "311910696"
+tempatlahir = "Karawang"
+umur = "20"
+telepon = "085609349306"
+alamat = "Dusun kerajan RT/RW 003/001 Desa solokan Kec.Pakisjaya Kab. Karawang"
+
+print("Assalamualaikum, \n let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,",My NPM is ",npm,". I was born in",tempatlahir," and I am ",umur,",years old. I an very glad if you want to invite my house in",alamat,". \n So, don't fotget to call me before with the number",telepon,"\n Thank you")
